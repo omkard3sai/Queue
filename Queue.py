5242465c317f59125d5e5151789adba2358ba779
omkard3sai <omkar.desai@utdallas.edu>
2017-06-18 17:54:44 -0500

    Replaced list() with node objects

--- Queue.py
+++ Queue.py
@@ -1,39 +1,44 @@
+from Node import Node
+
+
 class Queue:
 
-    def __init__(self, size):
-        self.queue = list()
-        self.size = size
+    def __init__(self):
+        self.head = None
 
     def push(self, item):
-        if not self.isfullstatus():
-            self.queue.insert(0, item)
-            return True
+        insert = Node(item)
+        if self.empty_status():
+            insert.previous = None
+            self.head = insert
         else:
-            return False
+            insert.previous = self.head
+            self.head = insert
 
     def pull(self):
-        if not self.isemptystatus():
-            return self.queue.pop()
+        if not self.empty_status():
+            pull_data = self.head.data
+            self.head = self.head.previous
+            return pull_data
         else:
             return False
 
     def peek(self):
-        if not self.isemptystatus():
-            return self.queue[0]
+        if not self.empty_status():
+            return self.head.data
         else:
             return False
 
-    def isfullstatus(self):
-        if len(self.queue) == self.size:
-            return True
-        else:
-            return False
-
-    def isemptystatus(self):
-        if not self.queue:
+    def empty_status(self):
+        if self.head is None:
             return True
         else:
             return False
 
     def print(self):
-        print('->', ','.join(str(a) for a in self.queue), '->')
+        queue = self.head
+        print("Rear/Push", end=" -> ")
+        while queue:
+            print(queue.data, end=" -> ")
+            queue = queue.previous
+        print("Front/Pull")
--- Sample.py
+++ Sample.py
@@ -1,12 +1,12 @@
 from Queue import Queue
 
-queueObject = Queue(5)
-queueObject.push(1)
-queueObject.push(2)
-queueObject.push(3)
-queueObject.push(4)
-queueObject.push(5)
-queueObject.push(6)
-queueObject.print()
-print("Run pull :: " + str(queueObject.pull()))
+queueObject = Queue()
+for i in range(6):
+    print("Run Push :: ", str(i+1))
+    queueObject.push(i+1)
+    if i == 4:
+        queueObject.print()
+print("Run Pull :: " + str(queueObject.pull()))
+print("Run Peek :: " + str(queueObject.peek()))
+print("Run Pull :: " + str(queueObject.pull()))
 queueObject.print()
